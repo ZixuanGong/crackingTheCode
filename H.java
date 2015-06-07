acafbbabb08872cf537296a7ecc75b9aa2e784c1
Zixuan Gong <zg2203@columbia.edu>
2015-06-07 12:08:08 -0700

    finish linkedlist part

--- H.java
+++ H.java
@@ -1,11 +1,38 @@
 public final class H {
-    public static Node str2list(String s) {
+    public static Node toCharList(String s) {
         Node head = new Node();
         Node p = head;
         for (char c: s.toCharArray()) {
             Node n = new Node(c);
             p.next = n;
             p = p.next;
+        }
+        return head.next;
+    }
+
+    public static Node toNumList(String s) {
+        Node head = new Node();
+        Node p = head;
+        for (char c: s.toCharArray()) {
+            Node n = new Node(Character.getNumericValue(c));
+            p.next = n;
+            p = p.next;
+        }
+        return head.next;
+    }
+
+
+    public static Node num2list(int n) {
+        Node head = new Node();
+        int val;
+
+        while (n > 0) {
+            val = n % 10;
+            n /= 10;
+
+            Node node = new Node(val);
+            node.next = head.next;
+            head.next = node;
         }
         return head.next;
     }
@@ -28,5 +55,34 @@ public final class H {
 
     public static boolean areEqual(Node p, Node q) {
         return p.d_char == q.d_char ? true : false;
+    }
+
+    public static void printCharList(Node head) {
+        Node p = head;
+        while (p != null) {
+            System.out.print(p.d_char + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+    public static void printNumList(Node head) {
+        Node p = head;
+        while (p != null) {
+            System.out.print(p.d_int + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+    public static int getLen(Node head) {
+        Node p = head;
+        int len = 0;
+
+        while (p != null) {
+            len++;
+            p = p.next;
+        }
+        return len;
     }
 }
--- notes_java.md
+++ notes_java.md
@@ -3,5 +3,8 @@
     char + int => int!! not string
     print array: Arrays.toString(arr)
 
+### Char
+    Character.getNumericValue(char)
+
 ### LinkedList
     .add(node) - add to tail
